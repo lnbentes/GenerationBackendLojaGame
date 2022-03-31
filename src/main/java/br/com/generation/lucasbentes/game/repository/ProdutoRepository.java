fcfa60ede24d9f49b253f377ff3d6c456ffa0430
lucas bentes <lucasbentes16@gmail.com>
2022-03-31 15:40:02 -0300

    correcao de bugs

--- src/main/java/br/com/generation/lucasbentes/game/repository/ProdutoRepository.java
+++ src/main/java/br/com/generation/lucasbentes/game/repository/ProdutoRepository.java
@@ -4,9 +4,11 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.generation.lucasbentes.game.model.Produto;
 
+@Repository 
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
 
 	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
